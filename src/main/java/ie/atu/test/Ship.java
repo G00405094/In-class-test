528b5f70ab7cded856222c7637a3d4f7efcb5243
G00405094 <G00405094@atu.ie>
2022-12-12 09:16:53 +0000

    Designed class Ship

---
+++ src/main/java/ie/atu/test/Ship.java
@@ -0,0 +1,40 @@
+package ie.atu.test;
+
+public class Ship {
+    private String name;
+    private String year;
+    private int crew;
+
+    public Ship() {
+    }
+
+    public Ship(String name, String year, int crew) {
+        this.name = name;
+        this.year = year;
+        this.crew = crew;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public int getCrew() {
+        return crew;
+    }
+
+    public void setCrew(int crew) {
+        this.crew = crew;
+    }
+}
---
+++ src/main/java/ie/atu/test/VessalApp.java
@@ -0,0 +1,7 @@
+package ie.atu.test;
+
+public class VessalApp {
+    public static void main(String[] args) {
+
+    }
+}
